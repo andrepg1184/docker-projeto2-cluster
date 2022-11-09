ad497a24605d2522593b27695e8df32f50582bf7
andrepg1184 <andreleite1184@hotmail.com>
2022-11-09 16:47:41 -0300

    Att IP

--- worker.sh
+++ worker.sh
@@ -1,1 +1,1 @@
-    docker swarm join --token SWMTKN-1-3pj8k0i4tn77bd93a0yxhgh36hxuef5q5oyg1732rztnfy29ll-a94q0ipwgrjs4xikzyb4yb3n5 10.10.10.100:2377
+    docker swarm join --token SWMTKN-1-3eix3nwj2gts45lyotrq2fo0jv459dncrfw4ju4or5lkor5ppj-2mmdabnhj6oy5uwe2s5yzgs0a 192.168.56.220:2377
